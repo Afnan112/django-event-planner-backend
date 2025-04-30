b693b75bacfa24aa65df6e6cccbdb05cbac5f395
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-04-30 10:43:20 +0300

    Create Event model

--- event_planner/models.py
+++ event_planner/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Event (models.Model):
+    title = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    location = models.CharField(max_length=255)
+    event_type = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
