73a0bfa0aa3d5d95a6611528a117c0123830b10d
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-04-30 11:59:59 +0300

    Create serializers.py

--- event_planner/views.py
+++ event_planner/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Event
+from .serializers import EventSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+class EventListView(APIView):
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data, status=200)
