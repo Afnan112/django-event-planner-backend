c405b585244526cdd689d63b9427d60b59f207be
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-05-01 23:25:10 +0300

    Add delete event api endpoint

--- event_planner/urls.py
+++ event_planner/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import EventListCreateView
+from .views import EventListCreateView, EventDetailView
 
 urlpatterns = [
-    path('events/', EventListCreateView.as_view(), name='event-list-create')
+    path('events/', EventListCreateView.as_view(), name='event-list-create'),
+    # api/all-event/int:event_id/
+    path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail')
 ]
--- event_planner/views.py
+++ event_planner/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from .models import Event
 from .serializers import EventSerializer
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class EventListCreateView(APIView):
@@ -19,3 +20,24 @@ class EventListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+class EventDetailView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Event, pk=pk)
+
+    def get(self, request, pk):
+        # Get the post object
+        # Serialize it with the PostSerializer
+        # Return it
+        event = self.get_object(pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data, status=200)
+    
+    def delete(self, request, pk):
+        # Get the post
+        # Delete the post
+        # Send an appropriate response
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=204)
