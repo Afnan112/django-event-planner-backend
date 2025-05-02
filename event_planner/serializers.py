f63bae3a00894e276eea7cfae97fcc677fe45a83
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-05-03 00:02:17 +0300

    Add serializers for attendance model

--- event_planner/serializers.py
+++ event_planner/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Event, Attendance
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = '__all__'   
