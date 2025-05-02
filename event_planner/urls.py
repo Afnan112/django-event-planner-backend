5670fd9437f95da1191799cd6060c8bbc63504db
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-05-03 01:21:34 +0300

    Change url path

--- event_planner/urls.py
+++ event_planner/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import EventListCreateView, EventDetailView, add_attendance
+from .views import EventListCreateView, EventDetailView, CreateAttendanceAPI
 
 urlpatterns = [
     path('events/', EventListCreateView.as_view(), name='event-list-create'),
     # api/all-event/int:event_id/
     path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
-    path('attendance/<int:event_id>/add-attendance/', add_attendance, name='add_attendance'),
-
+    path('event/<int:event_id>/add-attendance/', CreateAttendanceAPI.as_view(), name='api_add_attendance'),
 ]
