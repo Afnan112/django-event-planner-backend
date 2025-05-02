7f046b8c54121a7191772525995e3e0db71a62c6
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-05-03 00:37:21 +0300

    Add URL for Attendance

--- event_planner/urls.py
+++ event_planner/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import EventListCreateView, EventDetailView
+from .views import EventListCreateView, EventDetailView, add_attendance
 
 urlpatterns = [
     path('events/', EventListCreateView.as_view(), name='event-list-create'),
     # api/all-event/int:event_id/
-    path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail')
+    path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
+    path('attendance/<int:event_id>/add-attendance/', add_attendance, name='add_attendance'),
+
 ]
